db4f844ae698d0863e496b37c81751bfd079ccb1
Victor Tolbert <victor.tolbert@gmail.com>
2021-04-19 22:56:19 -0400

    display movies results in a table

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,36 @@
 export default {
   async asyncData({ $axios, $config: { tmdbApiKey } }) {
     const response = await $axios.$get(`/movie/top_rated?api_key=${tmdbApiKey}&language=en-US&page=1`)
-    return { response }
+    return {
+      movies: response.results,
+    }
   }
 }
 </script>
 
 <template>
-  <pre>{{ response }}</pre>
+  <table>
+    <thead>
+      <tr>
+        <th>Title</th>
+        <th>Vote Count</th>
+        <th>Vote Average</th>
+        <th>Popularity</th>
+        <th>Overview</th>
+        <th>Favorite</th>
+        <th>Delete</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="movie in movies" :key="movie.id">
+        <td>{{ movie.title }}</td>
+        <td>{{ movie.vote_count }}</td>
+        <td>{{ movie.vote_average }}</td>
+        <td>{{ movie.popularity }}</td>
+        <td>{{ movie.overview }}</td>
+        <td>Favorite</td>
+        <td>Delete</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
