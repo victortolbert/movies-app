36140dfb5e5eb31b48664bd0b93bd1853ab14bc3
Victor Tolbert <victor.tolbert@gmail.com>
2021-04-19 23:03:58 -0400

    add a little styling

--- pages/index.vue
+++ pages/index.vue
@@ -10,28 +10,30 @@ export default {
 </script>
 
 <template>
-  <table>
-    <thead>
-      <tr>
-        <th>Title</th>
-        <th>Vote Count</th>
-        <th>Vote Average</th>
-        <th>Popularity</th>
-        <th>Overview</th>
-        <th>Favorite</th>
-        <th>Delete</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="movie in movies" :key="movie.id">
-        <td>{{ movie.title }}</td>
-        <td>{{ movie.vote_count }}</td>
-        <td>{{ movie.vote_average }}</td>
-        <td>{{ movie.popularity }}</td>
-        <td>{{ movie.overview }}</td>
-        <td>Favorite</td>
-        <td>Delete</td>
-      </tr>
-    </tbody>
-  </table>
+  <main class="p-16 text-sm">
+    <table>
+      <thead>
+        <tr>
+          <th class="py-2 px-4 text-left">Title</th>
+          <th class="py-2 px-4 whitespace-nowrap">Vote Count</th>
+          <th class="py-2 px-4 whitespace-nowrap">Vote Average</th>
+          <th class="py-2 px-4">Popularity</th>
+          <th class="py-2 px-4 text-left">Overview</th>
+          <th class="py-2 px-4">Favorite</th>
+          <th class="py-2 px-4">Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="movie in movies" :key="movie.id">
+          <td class="align-top py-2 px-4 whitespace-nowrap">{{ movie.title }}</td>
+          <td class="align-top py-2 px-4 text-center">{{ movie.vote_count }}</td>
+          <td class="align-top py-2 px-4 text-center">{{ movie.vote_average }}</td>
+          <td class="align-top py-2 px-4">{{ movie.popularity }}</td>
+          <td class="align-top py-2 px-4">{{ movie.overview }}</td>
+          <td class="align-top py-2 px-4">Favorite</td>
+          <td class="align-top py-2 px-4">Delete</td>
+        </tr>
+      </tbody>
+    </table>
+  </main>
 </template>
